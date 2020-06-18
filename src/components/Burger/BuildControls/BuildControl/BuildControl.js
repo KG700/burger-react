011621390633abeb0615f1c2738ce933bfe2f639
Karla Gardiner <karla.gardiner@gmail.com>
2020-06-18 17:18:26 +0100

    BuildControls component now renders a list of BuildControl components

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import classes from './buildControl.module.css'
+import classes from './BuildControl.module.css'
 
 const buildControl = (props) => (
   <div className={classes.BuildControl}>
-    <div classname={classes.Label}>{props.label}</div>
+    <div className={classes.Label}>{props.label}</div>
     <button className={classes.Less}>-</button>
     <button className={classes.More}>+</button>
   </div>
