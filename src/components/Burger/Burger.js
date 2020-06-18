9f5b5197de908a4e956df20541188679707bb71c
Karla Gardiner <karla.gardiner@gmail.com>
2020-06-18 16:07:49 +0100

    Updated Burger component to render Ingredient components and BurgerBuilder to component ot render Burger

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import classes from './Burger.module.css';
+import Ingredient from './Ingredient/Ingredient';
+
+const burger = (props) => {
+  return (
+    <div className={classes.Burger}>
+      <Ingredient
+        type='bread-top'
+      />
+      <Ingredient
+        type='salad'
+      />
+      <Ingredient
+        type='bacon'
+      />
+      <Ingredient
+        type='cheese'
+      />
+      <Ingredient
+        type='meat'
+      />
+      <Ingredient
+        type='bread-bottom'
+      />
+    </div>
+  );
+};
+
+export default burger;
