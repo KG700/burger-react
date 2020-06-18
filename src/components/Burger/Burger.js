2c76ac23edfeac74588d1c4f8b90da0250fe055d
Karla Gardiner <karla.gardiner@gmail.com>
2020-06-18 16:23:38 +0100

    Updated BurgerBuilder state to save burger ingredients. Burger Component now accepts ingredients as props and renders appropriately

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,23 +4,21 @@ import classes from './Burger.module.css';
 import Ingredient from './Ingredient/Ingredient';
 
 const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(ingredientKey => {
+      return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+        return <Ingredient key={ingredientKey + i} type={ingredientKey} />;
+      });
+    });
+
   return (
     <div className={classes.Burger}>
       <Ingredient
         type='bread-top'
       />
-      <Ingredient
-        type='salad'
-      />
-      <Ingredient
-        type='bacon'
-      />
-      <Ingredient
-        type='cheese'
-      />
-      <Ingredient
-        type='meat'
-      />
+
+      {transformedIngredients}
+      
       <Ingredient
         type='bread-bottom'
       />
