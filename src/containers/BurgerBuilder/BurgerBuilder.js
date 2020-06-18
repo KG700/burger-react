2e99c6edaf0a58d08d77ff4b750d1af7de39fc2f
Karla Gardiner <karla.gardiner@gmail.com>
2020-06-18 17:38:46 +0100

    Added removeIngredientsHandler to remove ingredients from burger and keep track of total price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -41,7 +41,21 @@ class BurgerBuilder extends Component {
   }
 
   removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
 
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: newPrice
+    })
   }
 
   render () {
@@ -50,6 +64,7 @@ class BurgerBuilder extends Component {
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
           ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
         />
       </Aux>
     );
