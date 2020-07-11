c0986b10da3961b5c7ed14ee85540d3f5bb41ea7
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-11 13:19:51 +0100

    Rendering the Checkout on App.js. Some adjustments to styling made

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import classes from './CheckoutSummary.module.css';
 import Burger from '../../Burger/Burger';
-import Button from '../../UI/Button/Button;'
+import Button from '../../UI/Button/Button';
 
 const CheckoutSummary = (props) => {
   return (
     <div className={classes.CheckoutSummary}>
       <h1>We hope it tastes good!</h1>
-      <div style={{width: '300px', height: '300px', margin: 'auto'}}>
+      <div style={{width: '100%', margin: 'auto'}}>
         <Burger ingredients={props.ingredients} />
       </div>
       <Button
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import CheckoutSummary from '../../componets/Order/CheckoutSummary/CheckoutSummary';
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 
 class Checkout extends Component {
   state = {
