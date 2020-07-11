847303817226aa44745ee495970c4cd651f52ff6
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-11 17:55:17 +0100

    Updated orderHandler to update database with contactdata when form is submitted

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -67,9 +67,14 @@ class ContactData extends Component {
     event.preventDefault();
     // alert('You continue!');
     this.setState({ loading: true });
+    const formData = {};
+    for (let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
+    }
     const order = {
       ingredients: this.props.ingredients,
-      price: this.props.price
+      price: this.props.price,
+      order: formData
     }
     axios.post('/orders.json', order)
           .then( response => {
@@ -103,7 +108,7 @@ class ContactData extends Component {
     }
 
     let form = (
-      <form>
+      <form onSubmit={this.orderHandler}>
         {formElementsArray.map(formElement => (
           <Input
             key={formElement.id}
