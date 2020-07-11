4c31625714eb4028c641419cb5b9ce6c7f0a4dc5
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-11 13:34:58 +0100

    Updated purchaseContinueHandler, which handles the continue button in the orderSummary to direct user to the checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -97,29 +97,30 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    // alert('You continue!');
-    this.setState({ loading: true });
-    const order = {
-      indgredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Karla Gardiner',
-        address: {
-          street: 'Teststreet 1',
-          postCode: 'AB1 2CD',
-          country: 'UK'
-        },
-        email: 'test@test.com'
-      },
-      deliveryMethod: 'fastest'
-    }
-    axios.post('/orders.json', order)
-          .then( response => {
-            this.setState({ loading: false, purchasing: false });
-          })
-          .catch(error => {
-            this.setState({ loading: false, purchasing: false });
-          });
+  //   // alert('You continue!');
+  //   this.setState({ loading: true });
+  //   const order = {
+  //     indgredients: this.state.ingredients,
+  //     price: this.state.totalPrice,
+  //     customer: {
+  //       name: 'Karla Gardiner',
+  //       address: {
+  //         street: 'Teststreet 1',
+  //         postCode: 'AB1 2CD',
+  //         country: 'UK'
+  //       },
+  //       email: 'test@test.com'
+  //     },
+  //     deliveryMethod: 'fastest'
+  //   }
+  //   axios.post('/orders.json', order)
+  //         .then( response => {
+  //           this.setState({ loading: false, purchasing: false });
+  //         })
+  //         .catch(error => {
+  //           this.setState({ loading: false, purchasing: false });
+  //         });
+    this.props.history.push('/checkout');
   }
 
   render () {
