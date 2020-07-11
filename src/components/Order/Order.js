5299462602710125cbcdfd9dacf493ca9fc9cafd
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-11 15:25:58 +0100

    Passed price to Order to render

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -5,7 +5,7 @@ import classes from './Order.module.css';
 const Order = (props) => (
   <div className={classes.Order}>
     <p>Ingredients: Salad (1)</p>
-    <p>Price: <strong>£ 5.45</strong></p>
+    <p>Price: <strong>£ {props.price.toFixed(2)}</strong></p>
   </div>
 );
 
