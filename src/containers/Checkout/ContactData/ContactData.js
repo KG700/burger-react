e6b3e566b298616261b45e56bc900a6f32d59d24
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-11 16:47:42 +0100

    Added styling to Contact Data form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import axios from '../../../axios-orders';
 import classes from './ContactData.module.css';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
@@ -47,10 +48,10 @@ class ContactData extends Component {
   render () {
     let form = (
       <form>
-        <input className={classes.Input} type='text' name='name' placeholder='Your name' />
-        <input className={classes.Input} type='text' name='email' placeholder='Your Email' />
-        <input className={classes.Input} type='text' name='street' placeholder='Street' />
-        <input className={classes.Input} type='text' name='postCode' placeholder='Post Code' />
+        <Input inputtype='input' type='text' name='name' placeholder='Your name' />
+        <Input inputtype='input' type='text' name='email' placeholder='Your Email' />
+        <Input inputtype='input' type='text' name='street' placeholder='Street' />
+        <Input inputtype='input' type='text' name='postCode' placeholder='Post Code' />
         <Button
           btnType='Success'
           clicked={this.orderHandler}>ORDER</Button>
