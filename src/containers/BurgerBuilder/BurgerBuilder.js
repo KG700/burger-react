efb776b7ff8eeb98ded40c4caf56a21e601a952a
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-28 15:10:36 +0100

    Updated reducer with logic for adding ingredient

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -26,15 +26,15 @@ class BurgerBuilder extends Component {
     error: false
   }
 
-  componentWillMount () {
-    axios.get('/ingredients.json')
-          .then(response => {
-            this.setState({ ingredients: response.data })
-          })
-          .catch(error => {
-            this.setState({ error: true })
-          });
-  }
+  // componentWillMount () {
+  //   axios.get('/ingredients.json')
+  //         .then(response => {
+  //           this.setState({ ingredients: response.data })
+  //         })
+  //         .catch(error => {
+  //           this.setState({ error: true })
+  //         });
+  // }
 
   updatePurchaseState (ingredients) {
     const sum = Object.keys(ingredients)
