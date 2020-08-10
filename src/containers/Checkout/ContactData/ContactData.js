95944619306093e22d1da21fd8adaf7463b9180f
Karla Gardiner <karla.gardiner@gmail.com>
2020-08-10 18:42:33 +0100

    Added loading to order actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -92,8 +92,7 @@ class ContactData extends Component {
         valid: true
       },
     },
-    formIsValid: false,
-    loading: false,
+    formIsValid: false
   }
 
   orderHandler = (event) => {
@@ -183,7 +182,7 @@ class ContactData extends Component {
           clicked={this.orderHandler}>ORDER</Button>
       </form>
     );
-    if (this.state.loading) {
+    if (this.props.loading) {
       form = <Spinner />
     }
     return (
@@ -198,12 +197,15 @@ class ContactData extends Component {
 const mapStateToProps = state => {
   return {
     ings: state.ingredients,
-    price: state.totalPrice
+    price: state.totalPrice,
+    loading: state.loading
   }
 }
 
 const mapDispatchToProps = dispatch => {
-  onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+  return {
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
