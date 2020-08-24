f557ac94aa2c4c2fc864394a2baf08e359eb5d61
Karla Gardiner <karla.gardiner@gmail.com>
2020-08-24 17:57:42 +0100

    Added authentication to purchase burger process

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -108,7 +108,7 @@ class ContactData extends Component {
       price: this.props.price,
       order: formData
     }
-    this.props.onOrderBurger(order);
+    this.props.onOrderBurger(order, this.props.token);
 
   }
 
@@ -198,13 +198,14 @@ const mapStateToProps = state => {
   return {
     ings: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
-    loading: state.order.loading
+    loading: state.order.loading,
+    token: state.auth.token
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+    onOrderBurger: (orderData, token) => dispatch(actions.purchaseBurger(orderData, token))
   }
 }
 
